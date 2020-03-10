73c5910f8a3174657d3c532636a801125096516e
Omar Ziarmal <o.ziarmal@hotmail.com>
2020-03-10 15:14:18 +0100

    solution day 0 and 1

---
+++ hackerrank-30-days-of-code/day 01/solution.py
@@ -0,0 +1,18 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+e = 12
+f = 4.0
+g = 'is the best place to learn and practice coding!'
+# Read and save an integer, double, and String to your variables.
+e = int(input())
+f = float(input())
+g = str(input())
+# Print the sum of both integer variables on a new line.
+print(i+e)
+# Print the sum of the double variables on a new line.
+print(d+f)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+g)
